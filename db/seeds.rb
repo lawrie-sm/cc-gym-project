4777cccd9b02a16fbc1cebd688fdd63a7ce2111e
lawrie-sm <alertrelic@gmail.com>
2019-07-19 12:20:48 +0100

    Passed specific time into event

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
 require_relative '../models/event'
 require_relative '../models/member'
 
+spin_class_time = Time.new(2019, 10, 1, 12, 30)
 spin_class = Event.new(
   'name' => 'Spin Class',
   'description' => 'Cardio',
-  'time' => Time.now
+  'time' => spin_class_time
 )
 spin_class.save
--- models/specs/event_spec.rb
+++ models/specs/event_spec.rb
@@ -3,11 +3,12 @@ require_relative '../event'
 
 class TestEvent < MiniTest::Test
   def setup
+    @time = Time.now
     @event = Event.new(
       'id' => 1,
       'name' => 'Test Event',
       'description' => 'A test event',
-      'time' => '???'
+      'time' => @time
     )
   end
 
@@ -15,6 +16,6 @@ class TestEvent < MiniTest::Test
     assert_equal(@event.id, 1)
     assert_equal(@event.name, 'Test Event')
     assert_equal(@event.description, 'A test event')
-    assert_equal(@event.time, '???')
+    assert_equal(@event.time, @time)
   end
 end
