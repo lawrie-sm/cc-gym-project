e6657873f8e95fb52a9a3b2b0c3155dae9b68feb
lawrie-sm <alertrelic@gmail.com>
2019-07-19 14:15:47 +0100

    Added SQL for join table

--- db/gym.sql
+++ db/gym.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS members_events;
 DROP TABLE IF EXISTS members;
 DROP TABLE IF EXISTS events;
 
@@ -13,3 +14,9 @@ CREATE TABLE events (
   description TEXT NOT NULL,
   time TIMESTAMP NOT NULL
 );
+
+CREATE TABLE members_events (
+  ID SERIAL4 PRIMARY KEY,
+  member_id INT4 REFERENCES members (id),
+  event_id INT4 REFERENCES events (id)
+)
