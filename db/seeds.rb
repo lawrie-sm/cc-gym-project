cdae8be1e7c42e6ef4442741713fc220d8ab1f05
lawrie-sm <alertrelic@gmail.com>
2019-07-19 14:13:22 +0100

    Added some members to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,21 +2,32 @@ require_relative '../models/event'
 require_relative '../models/member'
 
 Event.delete_all
+Member.delete_all
 
 spin_class_time = Time.new(2019, 10, 1, 12, 30)
 spin_class = Event.new(
-  'name' => 'Spin Class',
-  'description' => 'Cardio',
+  'name' => 'spin class',
+  'description' => 'cardio',
   'time' => spin_class_time
 )
 spin_class.save
 
 lifting_class_time = Time.new(2019, 10, 2, 9, 0)
 lifting_class = Event.new(
-  'name' => 'Weightlifting Class',
-  'description' => 'Strength',
+  'name' => 'weightlifting class',
+  'description' => 'strength',
   'time' => lifting_class_time
 )
 lifting_class.save
 
-p Event.all
+arnold = Member.new(
+  'name' => 'arnold schwarzenegger',
+  'membership' => 'premium'
+)
+arnold.save
+
+conor = Member.new(
+  'name' => 'conor mcgregor',
+  'membership' => 'basic'
+)
+conor.save
