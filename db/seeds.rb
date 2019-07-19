14ab266cd1a9441ac0426af7a5b8bc931b01cb11
lawrie-sm <alertrelic@gmail.com>
2019-07-19 12:16:33 +0100

    Added event save and time stamp

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,9 @@
-p 'Seeds'
+require_relative '../models/event'
+require_relative '../models/member'
+
+spin_class = Event.new(
+  'name' => 'Spin Class',
+  'description' => 'Cardio',
+  'time' => Time.now
+)
+spin_class.save
--- models/event.rb
+++ models/event.rb
@@ -3,9 +3,18 @@ require_relative '../db/sql_runner'
 class Event
   attr_reader :id, :name, :description, :time
   def initialize(options)
-    @id = options['id'] if options['id']
+    @id = options['id'].to_i if options['id']
     @name = options['name']
     @description = options['description']
     @time = options['time']
+  end
+
+  def save
+    sql = '
+    INSERT INTO events (name, description, time)
+    VALUES ($1, $2, $3)
+    RETURNING id'
+    values = [@name, @description, @time]
+    @id = SqlRunner.run(sql, values).first['id'].to_i
   end
 end
