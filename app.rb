7c6e97283dbe7ac86550c436cda57d5f99a37b36
lawrie-sm <alertrelic@gmail.com>
2019-07-19 15:13:54 +0100

    Added basic view for events and controller

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 also_reload './models/*'
 require_relative 'controllers/members_controller'
+require_relative 'controllers/events_controller'
 
 get '/' do
   erb(:index)
