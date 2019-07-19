2fb5f0d9bad6f5fa24857f3953dd4b5b88bdfe5b
lawrie-sm <alertrelic@gmail.com>
2019-07-19 17:13:26 +0100

    Added a way to create new members

--- controllers/members_controller.rb
+++ controllers/members_controller.rb
@@ -5,6 +5,16 @@ get '/members' do
   erb(:'members/index')
 end
 
+get '/members/new' do
+  erb(:'members/new')
+end
+
+post '/members' do
+  member = Member.new(params)
+  member.save
+  redirect "/members/#{member.id}"
+end
+
 get '/members/:id' do
   @member = Member.find(params['id'])
   erb(:'members/show')
