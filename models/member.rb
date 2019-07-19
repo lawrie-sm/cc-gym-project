4c88f07d0f1e3d82c684c33576ff5e5e33586913
lawrie-sm <alertrelic@gmail.com>
2019-07-19 14:07:52 +0100

    Added some CRUD methods to member

--- models/member.rb
+++ models/member.rb
@@ -7,4 +7,29 @@ class Member
     @name = options['name']
     @membership = options['membership']
   end
+
+  def save
+    sql = '
+    INSERT INTO members (name, membership)
+    VALUES ($1, $2)
+    RETURNING id'
+    values = [@name, @membership]
+    @id = SqlRunner.run(sql, values).first['id'].to_i
+  end
+
+  def self.all
+    sql = 'SELECT * FROM members'
+    results = SqlRunner.run(sql)
+    return results.map { |e| Member.new(e) }
+  end
+
+  def self.delete(id)
+    sql = 'DELETE FROM members WHERE id = $1'
+    SqlRunner.run(sql, [id])
+  end
+
+  def self.delete_all
+    sql = 'DELETE FROM members'
+    SqlRunner.run(sql)
+  end
 end
